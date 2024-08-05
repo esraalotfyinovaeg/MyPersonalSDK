0b7f1494dd82489e123312351f5d6fa71ea18463
Esraa Lotfy <esraa.lotfy@inovaeg.com>
2024-08-05 13:26:07 +0300

    [Edit] Build project.

---
+++ Test.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  MyPersonalSDK
+//
+//  Created by Esraa Lotfy on 05/08/2024.
+//
+
+import Foundation
+
+public class GreetMySDK {
+    public init() {}
+    
+    public func greet(name: String) -> String {
+        return "Hello, \(name)! Welcome to MySimpleSDK."
+    }
+}
